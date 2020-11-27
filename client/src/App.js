f95b436b0aae5d5a2cf25c7aa488182c9ce84ab6
SOunit <workbookwork7@gmail.com>
2020-11-27 11:49:53 +0900

    added mongo message to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import NodeMessage from './containers/NodeMessage/NodeMessage';
+import MongoMessage from './containers/MongoMessage/MongoMessage';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
         <NodeMessage />
+        <MongoMessage />
       </header>
     </div>
   );
--- node/index.js
+++ node/index.js
@@ -7,4 +7,13 @@ app.get('/message', (req, res, next) => {
   res.send('hello from node, nodemon!');
 });
 
+app.get('/mongo/message', (req, res, next) => {
+  const message = new Message({ message: 'message from mongo!' });
+  message.save().then(() => {
+    Message.findOne({ _id: message._id }).then((msg) => {
+      res.send(msg);
+    });
+  });
+});
+
 app.listen(5000);
